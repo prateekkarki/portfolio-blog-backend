065f87a841174fe1fc4e1e0249b1d168642952ff
Prateek Karki <pratiek.karki@gmail.com>
2020-09-30 22:10:44 +0545

    initialize

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4d95b06c26ee807d122e803de09b0a5f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '61999e09-401d-4d30-9010-04752e2c597f'
+};
