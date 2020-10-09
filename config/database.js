e1d1e891419b7d67a55faa7adaeb74fca04dd8ca
Prateek Karki <pratiek.karki@gmail.com>
2020-10-10 00:12:10 +0545

    changing database to sqlite

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi-blog'),
-        username: env('DATABASE_USERNAME', 'user'),
-        password: env('DATABASE_PASSWORD', 'password'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      options: {}
-    },
-  },
+	defaultConnection: "default",
+	connections: {
+		default: {
+			connector: "bookshelf",
+			settings: {
+				client: "sqlite",
+				filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+			},
+			options: {
+				useNullAsDefault: true,
+			},
+		},
+	},
 });
