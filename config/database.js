090b8621bc58c4142cc69435b84c7c5a5d3fbee4
Prateek Karki <pratiek.karki@gmail.com>
2020-10-10 18:52:15 +0545

    moving production config to separate file

--- config/database.js
+++ config/database.js
@@ -3,21 +3,10 @@ module.exports = ({ env }) => ({
 	connections: {
 		default: {
 			connector: "bookshelf",
-			settings:
-				process.env.NODE_ENV === "production"
-					? {
-							client: "postgres",
-							host: env("DATABASE_HOST"),
-							port: env("DATABASE_PORT"),
-							database: env("DATABASE_NAME"),
-							username: env("DATABASE_USERNAME"),
-							password: env("DATABASE_PASSWORD"),
-							ssl: true,
-					  }
-					: {
-							client: "sqlite",
-							filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-					  },
+			settings: {
+				client: "sqlite",
+				filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+			},
 			options: {
 				useNullAsDefault: true,
 			},
