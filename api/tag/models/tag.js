7f6a2564f79d1fd46f0dfad36f9680fd263a1a16
Prateek Karki <pratiek.karki@gmail.com>
2020-10-10 17:45:37 +0545

    slugify tags and categories

--- api/tag/models/tag.js
+++ api/tag/models/tag.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
+const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+	lifecycles: {
+		async beforeCreate(data) {
+			data.slug = slugify(data.name, { lower: true });
+		},
+		async beforeUpdate(params, data) {
+			data.slug = slugify(data.name, { lower: true });
+		},
+	},
+};
