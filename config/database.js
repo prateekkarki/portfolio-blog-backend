492fdc6c85219553ee27cb4bba30c2c06f82dd55
Prateek Karki <pratiek.karki@gmail.com>
2020-10-10 17:06:32 +0545

    add postgres on production, adding production url

--- config/database.js
+++ config/database.js
@@ -3,10 +3,21 @@ module.exports = ({ env }) => ({
 	connections: {
 		default: {
 			connector: "bookshelf",
-			settings: {
-				client: "sqlite",
-				filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-			},
+			settings:
+				process.env.NODE_ENV === "production"
+					? {
+							client: "postgres",
+							host: env("DATABASE_HOST"),
+							host: env("DATABASE_PORT"),
+							database: env("DATABASE_NAME"),
+							username: env("DATABASE_USERNAME"),
+							password: env("DATABASE_PASSWORD"),
+							ssl: true,
+					  }
+					: {
+							client: "sqlite",
+							filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+					  },
 			options: {
 				useNullAsDefault: true,
 			},
