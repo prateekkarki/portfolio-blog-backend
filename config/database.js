b1485b3a2ef0bb2e398ff9489f4aafa22e4483a3
Prateek Karki <pratiek.karki@gmail.com>
2022-09-13 13:20:00 +0545

    strapi update

--- config/database.js
+++ config/database.js
@@ -4,12 +4,16 @@ module.exports = ({ env }) => ({
 		default: {
 			connector: "bookshelf",
 			settings: {
-				client: "sqlite",
-				filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+				client: "postgres",
+				host: `${process.env.DATABASE_HOST}`,
+				port: `${process.env.DATABASE_PORT}`,
+				database: `${process.env.DATABASE_NAME}`,
+				username: `${process.env.DATABASE_USERNAME}`,
+				password: `${process.env.DATABASE_PASSWORD}`,
+				ssl: { rejectUnauthorized: false },
+				schema: "public",
 			},
-			options: {
-				useNullAsDefault: true,
-			},
+			options: {},
 		},
 	},
 });
